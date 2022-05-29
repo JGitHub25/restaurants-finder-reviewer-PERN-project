b79ad043141e3f35150afa7b668be96f50ac8492
julian <Joolean14>
2022-05-28 21:45:09 -0500

    Install dotenv and nodemon, add mock e get route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express(); 
 
-app.listen(3005, () => {
-    console.log("Server is up a nd listening on port 3005");
+const port = process.env.PORT || 3001;
+
+app.get("/getRestaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        restaurant: "mcdonalds"
+    });
 })
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up and listening on port ${port}`);
+})
+
+
+
