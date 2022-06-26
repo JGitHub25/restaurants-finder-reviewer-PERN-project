b35689903adf70715d1dccebfe58067fefe2b101
julian <Joolean14>
2022-06-26 11:21:18 -0500

    Back and Front integration with console log output

--- client/src/pages/Home.page.js
+++ client/src/pages/Home.page.js
@@ -1,6 +1,22 @@
 import RestaurantsTable from "../components/RestaurantsTable";
+import axios from "axios";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+  
+  const fetchData = async (url) => {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  useEffect(() => {
+    fetchData("/api/v1/restaurants/")
+  }, [])
+  
   return (
     <>
       <h3>HOME</h3>
